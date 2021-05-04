89959c73024310d7a803c72e97a9bea9ec1172e1
MojtabaMoodi <mmoodi@uwaterloo.ca>
2021-05-04 19:22:46 -0400

    write functional tests.

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,3 +1,29 @@
+import hashlib
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 
-# Create your tests here.
+
+class FunctionalTestCase(TestCase):
+
+  def setUp(self):
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    self.browser = webdriver.Firefox(options=opts)
+
+  
+  def test_there_is_homepage(self):
+    self.browser.get('http://127.0.0.1:8000')
+    self.assertIn('Enter hash here', self.browser.page_source)
+
+  def test_hash_of_hello(self):
+    self.browser.get('http://127.0.0.1:8000')
+    text = self.browser.find_element_by_id('id_text')
+    text.send_keys('hello')
+    hashed_hello = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+    self.browser.find_element_by_name('submit').click()
+    self.assertIn(hashed_hello, self.browser.page_source)
+
+
+  def tearDown(self):
+    self.browser.quit()
