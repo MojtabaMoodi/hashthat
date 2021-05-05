f6d66aff1dc35a3d4743f10fea561088ebb2abae
MojtabaMoodi <mmoodi@uwaterloo.ca>
2021-05-05 00:33:09 -0400

    write a test for ajax method.

--- hashing/tests.py
+++ hashing/tests.py
@@ -5,6 +5,7 @@ from selenium.webdriver import FirefoxOptions
 from .forms import HashForm
 from .models import Hash
 from django.core.exceptions import ValidationError
+import time
 
 
 class FunctionalTestCase(TestCase):
@@ -25,6 +26,13 @@ class FunctionalTestCase(TestCase):
     hashed_hello = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
     self.browser.find_element_by_name('submit').click()
     self.assertIn(hashed_hello, self.browser.page_source)
+
+  def test_hash_ajax(self):
+    self.browser.get('http://127.0.0.1:8000')
+    text = self.browser.find_element_by_id('id_text')
+    text.send_keys('hello')
+    time.sleep(5) # Wait for AJAX
+    self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
   def tearDown(self):
     self.browser.quit()
